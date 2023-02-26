eddf6d331e97191de06facb7c57861c7a492b9c8
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-02-26 14:11:05 +0900

    feat: 각 module에 typeormmodule import

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { Admin } from './entities/admin.entity';
+import { Faq } from './entities/faq.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Admin, Faq])],
   controllers: [AdminController],
   providers: [AdminService]
 })
--- src/collections/collections.module.ts
+++ src/collections/collections.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CollectionsController } from './collections.controller';
 import { CollectionsService } from './collections.service';
+import { Collection } from './entities/collection.entity';
+import { Photospot } from './entities/photospot.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Collection, Photospot])],
   controllers: [CollectionsController],
   providers: [CollectionsService]
 })
