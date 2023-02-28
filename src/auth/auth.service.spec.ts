be514a12da06320ffb36949939eca2350792a84a
Park Jin <tstunas@gmail.com>
2023-02-28 20:51:15 +0900

    test: AuthService 의존성 모킹

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,12 +4,19 @@ import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { MailerAuthService } from 'src/mailer/service/mailer.auth.service';
+import { CACHE_MANAGER } from '@nestjs/common';
 
 const moduleMocker = new ModuleMocker(global);
 
 describe('AuthService', () => {
   let service: AuthService;
-  let mockUserRepository: jest.Mocked<Repository<User>>
+  let mockUserRepository: jest.Mocked<Repository<User>>;
+  let mockJwtService: jest.Mocked<JwtService>;
+  let mockConfigService: jest.Mocked<ConfigService>;
+  let mockMailerAuthService: jest.Mocked<MailerAuthService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,7 +25,12 @@ describe('AuthService', () => {
       .useMocker((token) => {
         if (token === getRepositoryToken(User)) {
           return {
-            findOneBy: jest.fn()
+            
+          };
+        }
+        if (token === CACHE_MANAGER) {
+          return {
+
           }
         }
         if (typeof token === 'function') {
@@ -30,7 +42,10 @@ describe('AuthService', () => {
       .compile();
 
     service = module.get(AuthService);
-    mockUserRepository = module.get(getRepositoryToken(User))
+    mockUserRepository = module.get(getRepositoryToken(User));
+    mockJwtService = module.get(JwtService);
+    mockConfigService = module.get(ConfigService);
+    mockMailerAuthService = module.get(MailerAuthService);
   });
 
   it('should be defined', () => {
