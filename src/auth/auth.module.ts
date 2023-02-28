b51225d55c13cd78a29ec38bfaece4d87e6f5303
Park Jin <tstunas@gmail.com>
2023-02-28 09:55:46 +0900

    Feat: 회원가입, 로그인 중간구현
    - bcrypt로 10번 salt를 돌려 암호화 저장
    - 임시로 기본 jwt secret 설정 (나중에 삭제)
    - 임시로 쿠키에 토큰 설정 (나중에는 바디에만 담아서 보낸다)
    - 샘플로 생성하는 User의 비밀번호를 bcrypt로 암호화한다.
    - AccessToken을 검증하는 JwtGuard를 사용하고, 토큰의 payload를 @InjectUser로 받는다. (추후 변경여지 있음)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), JwtModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
@@ -9,17 +9,24 @@ import {
   PutEmailVerificationBodyDTO,
   ChangePasswordBodyDTO,
 } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(User) private usersRepository: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+    private jwtService: JwtService,
+    private configService: ConfigService
+  ) {}
 
   async createSampleUser() {
     let arr = [];
     for (let i = 1; i <= 5; i++) {
       const temp = {
         email: `test_emai_${i}@gmail.com`,
-        password: 'qwer1234',
+        password: bcrypt.hashSync('qwer1234', 10),
       };
       arr.push(temp);
     }
@@ -28,39 +35,73 @@ export class AuthService {
 
   async signUp(body: SignUpBodyDTO) {
     const { email, password } = body;
+    const passwordHash = bcrypt.hashSync(password, 10);
+    await this.usersRepository.insert({ email, password: passwordHash });
     return {
-      message: 'message',
+      message: '회원가입 되었습니다.',
     };
   }
 
-  async signIn(body: SignInBodyDTO) {
+  async signIn(body: SignInBodyDTO, response: any) {
     const { email, password } = body;
+    const user = await this.usersRepository.findOneBy({ email });
+    if (!user) {
+      throw new NotFoundException({ message: '가입하지 않은 이메일입니다.' });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException({ message: '비밀번호가 일치하지 않습니다.' });
+    }
+    const accessToken = this.jwtService.sign(
+      {
+        id: user.id,
+        email: user.email,
+        role: 'user',
+      },
+      {
+        secret: this.configService.get('JWT_ACCESS_TOKEN_SECRET') || 'accessToken',
+        expiresIn: '1h',
+      }
+    );
+    const refreshToken = this.jwtService.sign(
+      {
+        id: user.id,
+      },
+      {
+        secret: this.configService.get('JWT_REFRESH_TOKEN_SECRET') || 'refreshToken',
+        expiresIn: '7d',
+      }
+    );
+    // 임시
+    response.cookie('accessToken', accessToken);
+    response.cookie('refreshToken', refreshToken);
+    return {
+      message: '로그인 되었습니다.',
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async signOut(user: any) {
     return {
       message: 'message',
     };
   }
 
-  async signOut() {
-    return {
-      message: 'message',
-    };
-  }
-
-  async postEmailVerification(body: PostEmailVerificationBodyDTO) {
+  async postEmailVerification(body: PostEmailVerificationBodyDTO, user: any) {
     const { email } = body;
     return {
       message: 'message',
     };
   }
 
-  async putEmailVerification(body: PutEmailVerificationBodyDTO) {
+  async putEmailVerification(body: PutEmailVerificationBodyDTO, user: any) {
     const { email } = body;
     return {
       message: 'message',
     };
   }
 
-  async changePassword(body: ChangePasswordBodyDTO) {
+  async changePassword(body: ChangePasswordBodyDTO, user: any) {
     const { password } = body;
     return {
       message: 'message',
