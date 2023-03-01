5d4ae4b6b0dcadc58b158c9ada254db6bc1ada21
muja-code <pla2697@gmail.com>
2023-03-01 18:36:51 +0900

    feat: 포토스팟들 가져오기, 포토스팟 가져오기, 포토스팟 수정하기 기능 추가

---
+++ src/photospot/dto/dist/modify-photospot.dto.js
@@ -0,0 +1,30 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.ModifyPhotospotDto = void 0;
+var decorators_1 = require("nestjs-form-data/dist/decorators");
+var class_validator_1 = require("class-validator");
+var ModifyPhotospotDto = /** @class */ (function () {
+    function ModifyPhotospotDto() {
+    }
+    __decorate([
+        class_validator_1.IsNotEmpty(),
+        class_validator_1.IsString()
+    ], ModifyPhotospotDto.prototype, "title");
+    __decorate([
+        class_validator_1.IsNotEmpty(),
+        class_validator_1.IsString()
+    ], ModifyPhotospotDto.prototype, "description");
+    __decorate([
+        class_validator_1.IsOptional(),
+        decorators_1.IsFile(),
+        decorators_1.HasMimeType(['image/jpeg', 'image/png'])
+    ], ModifyPhotospotDto.prototype, "image");
+    return ModifyPhotospotDto;
+}());
+exports.ModifyPhotospotDto = ModifyPhotospotDto;
