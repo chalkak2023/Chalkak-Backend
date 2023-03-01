1132d9c743299140f4a09b21b59de82e61c65624
boleesystem <boleesystem@gmail.com>
2023-03-01 11:02:12 +0900

    feat: 관리자 계정 목록 조회(검색, 페이지네이션 포함) 기능 추가

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { AdminService } from './admin.service';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  constructor(private adminService: AdminService) {}
+
+  @Get('auth')
+  async getAdminsList(@Req() req: Request) {
+    const result = await this.adminService.getAdminsList('admin');
+    if (req.query.keyword) {
+      result.where('admin.account LIKE :keyword OR admin.responsibility LIKE :keyword', { keyword: `%${req.query.keyword}` });
+    }
+
+    const p: number = parseInt(req.query.p as any) || 1;
+    const perPage = 6;
+    const total = await result.getCount();
+
+    result.offset((p - 1) * perPage).limit(perPage);
+
+    return {
+      data: await result.getMany(),
+      total,
+      p,
+      lastPage: Math.ceil(total / perPage),
+    };
+  }
+}
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Admin } from './entities/admin.entity';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(@InjectRepository(Admin) private adminRepository: Repository<Admin>) {}
+
+  async getAdminsList(alias: string) {
+    return this.adminRepository.createQueryBuilder(alias);
+  }
+}
