a43eeea0d0883a3cca28afb1d2fda30d845f180a
Park Jin <tstunas@gmail.com>
2023-03-02 16:18:01 +0900

    feat: JwtRefreshStrategy를 AuthModule에 등록

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,11 +5,12 @@ import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { MailerProviderModule } from 'src/mailer/mailer.module';
+import { JwtRefreshStrategy } from './guard/jwt-refresh/jwt-refresh.strategy';
 import { JwtStrategy } from './guard/jwt/jwt.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), JwtModule, CacheModule.register(), MailerProviderModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, JwtRefreshStrategy],
 })
 export class AuthModule {}
