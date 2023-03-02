62debe7700c7f4345b95f8c07f1fd248438634fb
muja-code <pla2697@gmail.com>
2023-03-02 15:49:06 +0900

    test: 포토스팟 서비스 테스트 코드 초기화

--- src/photospot/photospot.service.spec.ts
+++ src/photospot/photospot.service.spec.ts
@@ -1,88 +1,56 @@
-import { Param } from '@nestjs/common';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
-import * as sinon from 'sinon';
 import { Repository } from 'typeorm';
 import { FileSystemStoredFile } from 'nestjs-form-data/dist/classes/storage';
 import { NestjsFormDataModule } from 'nestjs-form-data';
 import { Test, TestingModule } from '@nestjs/testing';
-import { CreatePhotospotDto } from './dto/create-photospot.dto';
-import { PhotospotController } from './photospot.controller';
 import { PhotospotService } from './photospot.service';
 import { Photospot } from 'src/photospot/entities/photospot.entity';
 import { S3Service } from './../common/aws/s3.service';
 import { TypeOrmConfigService } from './../common/config/typeorm.config.service';
 
-describe.skip('PhotospotService', () => {
-  let photoService: PhotospotService;
-  let spyS3Service: S3Service;
-  let sandbox: sinon.SinonSandbox;
+const moduleMocker = new ModuleMocker(global);
 
-  beforeAll(async () => {
-    sandbox = sinon.createSandbox();
-    const S3ServiceProvider = {
-      provide: S3Service,
-      useFactory: () => ({
-        putObject: jest.fn(() => {}),
-      }),
-    };
+describe('PhotospotService', () => {
+  let service: PhotospotService;
+  let mockPhotospotRepository: jest.Mocked<Repository<Photospot>>;
+  let mockS3Service: jest.Mocked<S3Service>;
+  const REPOSITORY_TOKEN = getRepositoryToken(Photospot);
 
-    const app: TestingModule = await Test.createTestingModule({
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    const module: TestingModule = await Test.createTestingModule({
       imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
         TypeOrmModule.forFeature([Photospot]),
-        NestjsFormDataModule.config({
-          storage: FileSystemStoredFile,
-          autoDeleteFile: false,
-        }),
       ],
-      controllers: [PhotospotController],
-      providers: [
-        PhotospotService,
-        S3Service,
-        S3ServiceProvider,
-        {
-          provide: getRepositoryToken(Photospot),
-          useValue: sinon.createStubInstance(Repository),
-        },
-      ],
-    }).compile();
+      providers: [PhotospotService, S3Service],
+    })
+      .useMocker((token) => {
+        if (token === REPOSITORY_TOKEN) {
+          return {
+            insert: jest.fn(),
+            findOne: jest.fn(),
+            find: jest.fn(),
+            update: jest.fn(),
+          };
+        }
+        if (token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
-    photoService = app.get<PhotospotService>(PhotospotService);
-    spyS3Service = app.get<S3Service>(S3Service);
+    service = module.get(PhotospotService);
+    mockPhotospotRepository = module.get(REPOSITORY_TOKEN);
+    mockS3Service = module.get(S3Service);
   });
 
-  it('calling Service createPhotospot method', () => {
-    const dto = new CreatePhotospotDto();
-    const userId = 1;
-    const collectionId = 1;
-    expect(photoService.createPhotospot(dto, userId, collectionId)).not.toEqual(null);
-  });
-
-  it('calling S3Service putObject method', () => {
-    const dto = new CreatePhotospotDto();
-    const userId = 1;
-    const collectionId = 1;
-
-    photoService.createPhotospot(dto, userId, collectionId);
-    expect(spyS3Service.putObject).toHaveBeenCalled();
-    expect(spyS3Service.putObject).toHaveBeenCalledWith(dto.image);
-  });
-
-  it('calling Service getAllPhotospot method', () => {
-    const collectionId = 1;
-
-    expect(photoService.getAllPhotospot(collectionId)).not.toEqual(null);
-  });
-
-  it('calling Service getPhotospot method', () => {
-    const param = {collectionId: 1, photospotId: 1}
-
-    expect(photoService.getPhotospot(param)).not.toEqual(null);
-  })
-
-  afterAll(async () => {
-    sandbox.restore();
+  it('should be defined', () => {
+    expect(service).toBeDefined();
   });
 });
