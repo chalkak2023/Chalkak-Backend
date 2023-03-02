dbde35fac7ef78192998bc97ebeef98a37c6d5d2
muja-code <pla2697@gmail.com>
2023-03-02 15:08:49 +0900

    test: 포토스팟 컨트롤러 테스트 코드 초기화, createPhotospot 성공 테스트 코드 작성

--- src/photospot/photospot.controller.spec.ts
+++ src/photospot/photospot.controller.spec.ts
@@ -1,24 +1,18 @@
-import { FileSystemStoredFile } from 'nestjs-form-data/dist/classes/storage';
-import { NestjsFormDataModule } from 'nestjs-form-data';
-import { Test, TestingModule } from '@nestjs/testing';
 import { CreatePhotospotDto } from './dto/create-photospot.dto';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import { Test, TestingModule } from '@nestjs/testing';
 import { PhotospotController } from './photospot.controller';
 import { PhotospotService } from './photospot.service';
+import { FileSystemStoredFile, NestjsFormDataModule } from 'nestjs-form-data';
 
-describe.skip('PhotospotController', () => {
-  let photoController: PhotospotController;
-  let spyService: PhotospotService;
+const moduleMocker = new ModuleMocker(global);
 
-  beforeAll(async () => {
-    const PhotospotServiceProvider = {
-      provide: PhotospotService,
-      useFactory: () => ({
-        createPhotospot: jest.fn(() => {}),
-        getAllPhotospot: jest.fn(() => []),
-        getPhotospot: jest.fn(() => {}),
-      }),
-    };
-    const app: TestingModule = await Test.createTestingModule({
+describe('PhotospotController', () => {
+  let controller: PhotospotController;
+  let service: jest.Mocked<PhotospotService>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
       imports: [
         NestjsFormDataModule.config({
           storage: FileSystemStoredFile,
@@ -26,53 +20,37 @@ describe.skip('PhotospotController', () => {
         }),
       ],
       controllers: [PhotospotController],
-      providers: [PhotospotService, PhotospotServiceProvider],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
-    photoController = app.get<PhotospotController>(PhotospotController);
-    spyService = app.get<PhotospotService>(PhotospotService);
+    controller = module.get(PhotospotController);
+    service = module.get(PhotospotService);
   });
 
-  it('calling Controller createPhotospot method', () => {
-    const dto = new CreatePhotospotDto();
-    const collectionId = 1;
-    expect(photoController.createPhotospot(dto, collectionId)).not.toEqual(null);
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
   });
 
-  it('calling Service createPhotospot method', () => {
-    const dto = new CreatePhotospotDto();
-    const userId = 1;
-    const collectionId = 1;
+  describe('POST createPhotospot', () => {
+    it('should be defined', () => {
+      expect(controller.createPhotospot).toBeDefined();
+    });
 
-    photoController.createPhotospot(dto, collectionId);
-    expect(spyService.createPhotospot).toHaveBeenCalled();
-    expect(spyService.createPhotospot).toHaveBeenCalledWith(dto, userId, collectionId);
+    it('createPhotospot 성공', () => {
+      const dto = new CreatePhotospotDto();
+      const userId = 1;
+      const collectionId = 1;
+
+      service.createPhotospot(dto, userId, collectionId);
+      expect(service.createPhotospot).toHaveBeenCalledTimes(1);
+      expect(service.createPhotospot).toHaveBeenCalledWith(dto, userId, collectionId);
+    });
   });
-
-  it('calling Controller getAllPhotospot method', () => {
-    const collectionId = 1;
-    expect(photoController.getAllPhotospot(collectionId)).not.toEqual(null);
-  });
-
-  it('calling Service getAllPhotospot method', () => {
-    const collectionId = 1;
-
-    photoController.getAllPhotospot(collectionId);
-    expect(spyService.getAllPhotospot).toHaveBeenCalled();
-    expect(spyService.getAllPhotospot).toHaveBeenCalledWith(collectionId);
-  });
-
-  it('calling Controller getPhotospot method', () => {
-    const param = { collectionId: 1, photospotId: 1 };
-
-    expect(photoController.getPhotospot(param)).not.toEqual(null);
-  });
-
-  it('calling Service getPhotospot method', () => {
-    const param = { collectionId: 1, photospotId: 1 };
-
-    photoController.getPhotospot(param);
-    expect(spyService.getPhotospot).toHaveBeenCalled();
-    expect(spyService.getPhotospot).toHaveBeenCalledWith(param);
-  })
 });
