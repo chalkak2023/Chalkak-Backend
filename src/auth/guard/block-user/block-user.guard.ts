372f133bf47798c56bc0bd4be386ebfc3c840c11
Park Jin <tstunas@gmail.com>
2023-03-06 12:36:56 +0900

    feat: 캐시매니저를 사용하여 유저 조회 횟수를 최소화
    - 유저를 블락할 때에 캐시매니저에 등록되도록 하면 좋을 듯
    - 5분 제한

--- src/auth/guard/block-user/block-user.guard.ts
+++ src/auth/guard/block-user/block-user.guard.ts
@@ -13,12 +13,24 @@ export class BlockUserGuard implements CanActivate {
       user: { id: userId },
     } = ctx.switchToHttp().getRequest();
     const usersRepository = this.dataSource.getRepository(User);
+    const isBlock = await this.cacheManager.get<boolean>(`user-${userId}-block`);
+    if (!_.isNil(isBlock)) {
+      if (!isBlock) {
+        throw new ForbiddenException({
+          message: '블락된 상태여서 사용할 수 없습니다.'
+        })
+      }
+      return isBlock;
+    }
     const user = await usersRepository.findOne({ where: { id: userId } });
     if (_.isNil(user)) {
       throw new UnauthorizedException({
         message: '존재하지 않는 회원입니다.',
       });
     }
+    await this.cacheManager.set<boolean>(`user-${userId}-block`, user.isBlock, {
+      ttl: 1000 * 60 * 5
+    })
     if (user.isBlock) {
       throw new ForbiddenException({
         message: '블락된 상태여서 사용할 수 없습니다.'
