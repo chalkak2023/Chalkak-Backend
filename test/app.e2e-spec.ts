ccf127b0817e855b3227aef7127cc69d281a386c
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-03-06 17:53:13 +0900

    #86 test: e2e test code 필요없는 코드 제거 및 샘플 코드 추가

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 // import * as request from 'supertest';
 import request from 'supertest';
-import { AppModule } from '../src/app.module';
-import cookieParser from 'cookie-parser';
-import { SignUpBodyDTO } from 'src/auth/dto/auth.dto';
+import { AppModule } from './../src/app.module';
+import { decodedAccessTokenDTO, SignInBodyDTO, SignUpBodyDTO } from 'src/auth/dto/auth.dto';
 import { Connection } from 'typeorm';
+import { CreateMeetupDTO } from 'src/meetups/dto/create-meetup.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -16,18 +16,6 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    app.enableCors({
-      origin: true,
-      credentials: true,
-    });
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-      })
-    );
-    app.use(cookieParser());
     const connection = app.get(Connection);
     await connection.synchronize(true);
     await app.init();
@@ -46,28 +34,107 @@ describe('AppController (e2e)', () => {
       .expect('Chalkak');
   });
 
-  describe('/api/auth', () => {
-    const signupBodyDTO: SignUpBodyDTO = {
-      email: 'test@gmail.com',
-      password: 'qwer1234',
-    }
-    it('POST 201', () => {
-      return request(app.getHttpServer())
-      .post('/api/auth/signup')
-      .send(signupBodyDTO)
-      .expect(201).expect({
-        message: '회원가입 되었습니다.',
+  describe('auth', () => {
+    describe('POST /api/auth/signup - 회원가입', () => {
+      it('Success', () => {
+        const signupBodyDTO: SignUpBodyDTO = {
+          email: 'test@gmail.com',
+          password: 'qwer1234',
+        }
+        return request(app.getHttpServer())
+        .post('/api/auth/signup')
+        .send(signupBodyDTO)
+        .expect(201).expect({
+          message: '회원가입 되었습니다.',
+        });
+      });
+      it('Fail - Bad Request', () => {
+        const signupBodyDTO: Object = {
+          email: 'test@gmail.com',
+          password: 'qwer1234',
+          wrong: 'wrong',
+        }
+        return request(app.getHttpServer())
+        .post('/api/auth/signup')
+        .send(signupBodyDTO)
+        .expect(400);
+      });
+    });
+
+    describe('POST /api/auth/signin - 로그인', () => {
+      it('Success', () => {
+        const signinBodyDTO: SignInBodyDTO = {
+          email: 'test@gmail.com',
+          password: 'qwer1234',
+        }
+        return request(app.getHttpServer())
+        .post('/api/auth/signin')
+        .send(signinBodyDTO)
+        .expect(200);
+      });
+      it('Fail - Not signup email', () => {
+        const signinBodyDTO: SignInBodyDTO = {
+          email: 'wrong@gmail.com',
+          password: 'qwer1234',
+        }
+        return request(app.getHttpServer())
+        .post('/api/auth/signin')
+        .send(signinBodyDTO)
+        .expect(404)
+        .expect({
+          message: '가입하지 않은 이메일입니다.',
+        });
+      });
+      it('Fail - Wrong password', () => {
+        const signinBodyDTO: SignInBodyDTO = {
+          email: 'test@gmail.com',
+          password: 'wrong_pw',
+        }
+        return request(app.getHttpServer())
+        .post('/api/auth/signin')
+        .send(signinBodyDTO)
+        .expect(401)
+        .expect({
+          message: '비밀번호가 일치하지 않습니다.',
+        });
       });
     });
   });
 
-  describe('/api/meetups', () => {
-    it('GET 200', () => {
-      // localhost:3000 안쓰기 위해 app.getHttpServer() 시용
-      return request(app.getHttpServer())
-      .get('/api/meetups')
-      .expect(200)
-      .expect([]);
+  describe('meetups', () => {
+    describe('GET /api/meetups - 모임 목록 조회', () => {
+      it('Success', () => {
+        return request(app.getHttpServer())
+        .get('/api/meetups')
+        .expect(200)
+        .expect([]);
+      });
     });
+    // describe('POST /api/meetups - 모임 등록', () => {
+    //   it('Success', () => {
+    //     const meetupDTO: CreateMeetupDTO = {
+    //       title: 'title',
+    //       content: 'content',
+    //       place: 'place',
+    //       schedule: '2023-03-02 20:00',
+    //       headcount: 2
+    //     }
+    //     const userDTO: decodedAccessTokenDTO = {
+    //       id: 1,
+    //       username: '',
+    //       role: 'user',
+    //       iat: 0,
+    //       exp: 0
+    //     }
+    //     return request(app.getHttpServer())
+    //     .post('/api/meetups')
+    //     .send(meetupDTO)
+    //     .set('Cookie', [  // TODO: 로그인 인증하는 과정에서 해결 못함.
+    //       'accessToken=accessToken', 
+    //       'refreshToken=refreshToken' 
+    //     ])
+    //     .expect(201);
+    //   });
+    // });
   });
 });
