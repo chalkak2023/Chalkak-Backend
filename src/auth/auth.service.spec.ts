7daa9efcccf21af4d0c95a43691ee899edbae5a9
Park Jin <tstunas@gmail.com>
2023-03-07 12:06:00 +0900

    test: 삭제해야할 createSampleUser를 제외하고 모두 인증 유닛 테스트는 커버리지 전부

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -10,6 +10,7 @@ import { MailerAuthService } from 'src/mailer/service/mailer.auth.service';
 import {
   BadRequestException,
   CACHE_MANAGER,
+  ForbiddenException,
   InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
@@ -68,6 +69,7 @@ describe('AuthService', () => {
     users = [
       {
         id: 1,
+        username: 'test',
         email: 'test@gmail.com',
         password: 'testpassword|10',
         isBlock: false,
@@ -77,9 +79,20 @@ describe('AuthService', () => {
       },
       {
         id: 2,
+        username: 'test1',
         email: 'test@naver.com',
-        password: 'testpass|10',
+        password: 'testpassword|10',
         isBlock: false,
+        createdAt: new Date('2023-02-28T13:50:22.225Z'),
+        updatedAt: new Date('2023-02-28T13:50:32.144Z'),
+        deletedAt: new Date('2023-02-28T13:53:03.387Z'),
+      },
+      {
+        id: 3,
+        username: 'test3',
+        email: 'test3@naver.com',
+        password: 'testpassword|10',
+        isBlock: true,
         createdAt: new Date('2023-02-28T13:50:22.225Z'),
         updatedAt: new Date('2023-02-28T13:50:32.144Z'),
         deletedAt: new Date('2023-02-28T13:53:03.387Z'),
@@ -177,10 +190,10 @@ describe('AuthService', () => {
       };
 
       mockLocalUserRepository.insert.mockResolvedValue({
-        generatedMaps: [{ id: 3 }],
+        generatedMaps: [{ id: 4 }],
         identifiers: [
           {
-            id: 3,
+            id: 4,
             isBlock: false,
             createdAt: new Date('2023-03-06T15:01:25.962Z'),
             updatedAt: new Date('2023-03-06T15:01:25.962Z'),
@@ -190,7 +203,7 @@ describe('AuthService', () => {
         raw: {
           fieldCount: 0,
           affectedRows: 1,
-          insertId: 3,
+          insertId: 4,
           serverStatus: 2,
           warningCount: 0,
           message: '',
@@ -204,15 +217,15 @@ describe('AuthService', () => {
       });
     });
 
-    it('should be return fail message when username error situation', async () => {
-      const { username } = users[0]
+    it('should be return fail message when username error situation', () => {
+      const { username } = users[0];
       const body: SignUpBodyDTO = {
         username,
         email: 'test5@gmail.com',
         password: 'qwer1234',
       };
 
-      mockUserRepository.findOne.mockResolvedValueOnce(users[0]);
+      mockUserRepository.findOne.mockResolvedValue(users[0]);
 
       expect(service.signUp(body)).rejects.toThrowError(
         new BadRequestException({
@@ -262,6 +275,53 @@ describe('AuthService', () => {
         accessToken,
         refreshToken,
       });
+    });
+
+    it('should throw NotFoundException when email is not correct', async () => {
+      const body: SignInBodyDTO = {
+        email: 'fake' + users[0].email,
+        password: 'testpassword',
+      };
+      const response: any = {
+        cookie: jest.fn(() => response),
+      };
+      mockLocalUserRepository.findOne.mockResolvedValue(null);
+
+      expect(service.signIn(body, response)).rejects.toThrowError(
+        new NotFoundException({ message: '이메일이나 비밀번호가 일치하지 않습니다.' })
+      );
+    });
+
+    it('should throw NotFoundException when password is not correct', async () => {
+      const body: SignInBodyDTO = {
+        email: users[0].email,
+        password: 'faketestpassword',
+      };
+      const response: any = {
+        cookie: jest.fn(() => response),
+      };
+      mockLocalUserRepository.findOne.mockResolvedValue(users[0]);
+
+      expect(service.signIn(body, response)).rejects.toThrowError(
+        new NotFoundException({ message: '이메일이나 비밀번호가 일치하지 않습니다.' })
+      );
+    });
+
+    it('should throw ForbiddenException when user is block', async () => {
+      const body: SignInBodyDTO = {
+        email: users[2].email,
+        password: 'testpassword',
+      };
+      const response: any = {
+        cookie: jest.fn(() => response),
+      };
+      mockLocalUserRepository.findOne.mockResolvedValue(users[2]);
+
+      expect(service.signIn(body, response)).rejects.toThrowError(
+        new ForbiddenException({
+          message: '블락된 상태여서 로그인할 수 없습니다.',
+        })
+      );
     });
   });
 
@@ -316,12 +376,44 @@ describe('AuthService', () => {
       };
 
       cache = {
-        'test@gmail.com_verifyToken': 123456,
+        [`${body.email}_verifyToken`]: 123456,
       };
 
       expect(service.putEmailVerification(body)).resolves.toStrictEqual({
         message: '이메일 인증번호가 확인되었습니다.',
       });
+    });
+
+    it('should throw NotFoundException when verifyToken is not cached', async () => {
+      const body = {
+        email: 'test@gmail.com',
+        verifyToken: 123456,
+      };
+
+      cache = {};
+
+      expect(service.putEmailVerification(body)).rejects.toThrowError(
+        new NotFoundException({
+          message: '인증번호를 요청하지 않았거나 만료되었습니다.',
+        })
+      );
+    });
+
+    it('should throw UnauthorizedException when verifyToken is different with cachedVerifyToken', async () => {
+      const body = {
+        email: 'test@gmail.com',
+        verifyToken: 123456,
+      };
+
+      cache = {
+        [`${body.email}_verifyToken`]: 123458,
+      };
+
+      expect(service.putEmailVerification(body)).rejects.toThrowError(
+        new UnauthorizedException({
+          message: '인증번호가 일치하지 않습니다.',
+        })
+      );
     });
   });
 
@@ -362,11 +454,8 @@ describe('AuthService', () => {
       };
       const nickname = 'test-nickname';
       const providerUserId = 1;
-      mockUserRepository.findOne.mockResolvedValue(null);
-      mockNaverUserRepository.findOne.mockResolvedValue({
-        id: 1,
-        username: 'test',
-      } as NaverUser);
+      mockUserRepository.findOne.mockResolvedValueOnce(null);
+      mockNaverUserRepository.findOne.mockResolvedValue(users[0]);
       mockSocialNaverService.getOauth2Token.mockResolvedValue({ access_token: 'accessToken' });
       mockSocialNaverService.getUserInfo.mockResolvedValue({ id: providerUserId, nickname });
       mockJwtService.sign.mockImplementation((payload: any, options: any) => {
@@ -390,11 +479,8 @@ describe('AuthService', () => {
       };
       const nickname = 'test-nickname';
       const providerUserId = 1;
-      mockUserRepository.findOne.mockResolvedValue(null);
-      mockKakaoUserRepository.findOne.mockResolvedValue({
-        id: 1,
-        username: 'test',
-      } as KakaoUser);
+      mockUserRepository.findOne.mockResolvedValueOnce(null);
+      mockKakaoUserRepository.findOne.mockResolvedValue(users[0]);
       mockSocialKakaoService.getOauth2Token.mockResolvedValue({ access_token: 'accessToken' });
       mockSocialKakaoService.getUserInfo.mockResolvedValue({ id: providerUserId, kakao_account: { profile: { nickname } } });
       mockJwtService.sign.mockImplementation((payload: any, options: any) => {
@@ -408,6 +494,159 @@ describe('AuthService', () => {
         accessToken,
         refreshToken,
       });
+    });
+
+    it('should be return success message when username is exist', async () => {
+      const provider: 'kakao' | 'naver' = 'naver';
+      const body: SocialLoginBodyDTO = {
+        code: 'test',
+        state: 'chalkak',
+      };
+      const nickname = users[0].username;
+      const providerUserId = 1;
+      mockUserRepository.findOne.mockResolvedValueOnce(users[1]);
+      mockNaverUserRepository.findOne.mockResolvedValue(users[0]);
+      mockSocialNaverService.getOauth2Token.mockResolvedValue({ access_token: 'accessToken' });
+      mockSocialNaverService.getUserInfo.mockResolvedValue({ id: providerUserId, nickname });
+      mockJwtService.sign.mockImplementation((payload: any, options: any) => {
+        return `token${options.secret}`;
+      });
+      const accessToken = `token${mockConfigService.get('JWT_ACCESS_TOKEN_SECRET')}`;
+      const refreshToken = `token${mockConfigService.get('JWT_REFRESH_TOKEN_SECRET')}`;
+
+      expect(service.oauthSignIn(provider, body)).resolves.toStrictEqual({
+        message: '로그인되었습니다.',
+        accessToken,
+        refreshToken,
+      });
+    });
+
+    it('should be return success message when naver is not signup', async () => {
+      const provider: 'kakao' | 'naver' = 'naver';
+      const body: SocialLoginBodyDTO = {
+        code: 'test',
+        state: 'chalkak',
+      };
+      const nickname = 'test-nickname';
+      const providerUserId = 1;
+      mockUserRepository.findOne.mockResolvedValueOnce(null);
+      mockNaverUserRepository.findOne.mockResolvedValueOnce(null);
+      mockNaverUserRepository.findOne.mockResolvedValueOnce(users[0]);
+      mockSocialNaverService.getOauth2Token.mockResolvedValue({ access_token: 'accessToken' });
+      mockSocialNaverService.getUserInfo.mockResolvedValue({ id: providerUserId, nickname });
+      mockJwtService.sign.mockImplementation((payload: any, options: any) => {
+        return `token${options.secret}`;
+      });
+      const accessToken = `token${mockConfigService.get('JWT_ACCESS_TOKEN_SECRET')}`;
+      const refreshToken = `token${mockConfigService.get('JWT_REFRESH_TOKEN_SECRET')}`;
+
+      expect(service.oauthSignIn(provider, body)).resolves.toStrictEqual({
+        message: '로그인되었습니다.',
+        accessToken,
+        refreshToken,
+      });
+    });
+
+    it('should be return success message when kakao is not signup', async () => {
+      const provider: 'kakao' | 'naver' = 'kakao';
+      const body: SocialLoginBodyDTO = {
+        code: 'test',
+        state: 'chalkak',
+      };
+      const nickname = 'test-nickname';
+      const providerUserId = 1;
+      mockUserRepository.findOne.mockResolvedValueOnce(null);
+      mockKakaoUserRepository.findOne.mockResolvedValueOnce(null);
+      mockKakaoUserRepository.findOne.mockResolvedValueOnce(users[0]);
+      mockSocialKakaoService.getOauth2Token.mockResolvedValue({ access_token: 'accessToken' });
+      mockSocialKakaoService.getUserInfo.mockResolvedValue({ id: providerUserId, kakao_account: { profile: { nickname } } });
+      mockJwtService.sign.mockImplementation((payload: any, options: any) => {
+        return `token${options.secret}`;
+      });
+      const accessToken = `token${mockConfigService.get('JWT_ACCESS_TOKEN_SECRET')}`;
+      const refreshToken = `token${mockConfigService.get('JWT_REFRESH_TOKEN_SECRET')}`;
+
+      expect(service.oauthSignIn(provider, body)).resolves.toStrictEqual({
+        message: '로그인되었습니다.',
+        accessToken,
+        refreshToken,
+      });
+    });
+
+    
+    it('should be return success message when naver insert fail', async () => {
+      const provider: 'kakao' | 'naver' = 'naver';
+      const body: SocialLoginBodyDTO = {
+        code: 'test',
+        state: 'chalkak',
+      };
+      const nickname = 'test-nickname';
+      const providerUserId = 1;
+      mockUserRepository.findOne.mockResolvedValueOnce(null);
+      mockNaverUserRepository.findOne.mockResolvedValueOnce(null);
+      mockNaverUserRepository.insert.mockRejectedValue(new Error())
+      mockNaverUserRepository.findOne.mockResolvedValueOnce(users[0]);
+      mockSocialNaverService.getOauth2Token.mockResolvedValue({ access_token: 'accessToken' });
+      mockSocialNaverService.getUserInfo.mockResolvedValue({ id: providerUserId, nickname });
+      mockJwtService.sign.mockImplementation((payload: any, options: any) => {
+        return `token${options.secret}`;
+      });
+      const accessToken = `token${mockConfigService.get('JWT_ACCESS_TOKEN_SECRET')}`;
+      const refreshToken = `token${mockConfigService.get('JWT_REFRESH_TOKEN_SECRET')}`;
+
+      expect(service.oauthSignIn(provider, body)).rejects.toThrowError( new BadRequestException({
+        message: '가입에 실패했습니다.',
+      }));
+    });
+
+    it('should be return success message when kakao insert fail', async () => {
+      const provider: 'kakao' | 'naver' = 'kakao';
+      const body: SocialLoginBodyDTO = {
+        code: 'test',
+        state: 'chalkak',
+      };
+      const nickname = 'test-nickname';
+      const providerUserId = 1;
+      mockUserRepository.findOne.mockResolvedValueOnce(null);
+      mockKakaoUserRepository.findOne.mockResolvedValueOnce(null);
+      mockKakaoUserRepository.insert.mockRejectedValue(new Error())
+      mockKakaoUserRepository.findOne.mockResolvedValueOnce(users[0]);
+      mockSocialKakaoService.getOauth2Token.mockResolvedValue({ access_token: 'accessToken' });
+      mockSocialKakaoService.getUserInfo.mockResolvedValue({ id: providerUserId, kakao_account: { profile: { nickname } } });
+      mockJwtService.sign.mockImplementation((payload: any, options: any) => {
+        return `token${options.secret}`;
+      });
+      const accessToken = `token${mockConfigService.get('JWT_ACCESS_TOKEN_SECRET')}`;
+      const refreshToken = `token${mockConfigService.get('JWT_REFRESH_TOKEN_SECRET')}`;
+
+      expect(service.oauthSignIn(provider, body)).rejects.toThrowError( new BadRequestException({
+        message: '가입에 실패했습니다.',
+      }));
+    });
+
+    
+    it('should be return success message when user is block', async () => {
+      const provider: 'kakao' | 'naver' = 'kakao';
+      const body: SocialLoginBodyDTO = {
+        code: 'test',
+        state: 'chalkak',
+      };
+      const nickname = 'test-nickname';
+      const providerUserId = 1;
+      mockUserRepository.findOne.mockResolvedValueOnce(null);
+      mockKakaoUserRepository.findOne.mockResolvedValueOnce(null);
+      mockKakaoUserRepository.findOne.mockResolvedValueOnce(users[2]);
+      mockSocialKakaoService.getOauth2Token.mockResolvedValue({ access_token: 'accessToken' });
+      mockSocialKakaoService.getUserInfo.mockResolvedValue({ id: providerUserId, kakao_account: { profile: { nickname } } });
+      mockJwtService.sign.mockImplementation((payload: any, options: any) => {
+        return `token${options.secret}`;
+      });
+      const accessToken = `token${mockConfigService.get('JWT_ACCESS_TOKEN_SECRET')}`;
+      const refreshToken = `token${mockConfigService.get('JWT_REFRESH_TOKEN_SECRET')}`;
+
+      expect(service.oauthSignIn(provider, body)).rejects.toThrowError( new ForbiddenException({
+        message: '블락된 상태여서 로그인할 수 없습니다.',
+      }));
     });
   });
 
