db1a51ea914448a420ef38e0f655a8e8b5cd03d2
boleesystem <boleesystem@gmail.com>
2023-03-14 01:17:58 +0900

    fix: getCollectionsList 내 search, userId 관련 if문 코드 분리 및 alias 적용

--- src/collections/collection.user.keyword.repository.ts
+++ src/collections/collection.user.keyword.repository.ts
@@ -10,54 +10,46 @@ export class CollectionUserKeywordRepository extends Repository<Collection> {
     super(Collection, dataSource.createEntityManager());
   }
 
-  async getCollectionsList({ search, p, userId }: GetCollectionsListQueryDto) {
+  async getCollectionsList({ p, search, userId }: GetCollectionsListQueryDto) {
     const take = 18;
-    const collectionsList = this.createQueryBuilder('collection');
-    let myCollectionQuery = 'collection.userId = :userId';
-    let searchCollectionQuery = 'collection.title LIKE :search OR collection.description LIKE :search';
-
-    if (!search && !userId) {
-      collectionsList;
-    } else if (search && !userId) {
-      collectionsList.where(searchCollectionQuery, { search: `%${search}%` });
-    } else if (!search && userId) {
-      collectionsList.where(myCollectionQuery, { userId });
-    } else {
-      collectionsList.where(`${myCollectionQuery} AND (${searchCollectionQuery})`, { userId, search: `%${search}%` });
-    }
-
-    return await collectionsList
-      .select([
-        'collection.id',
-        'collection.userId',
-        'collection.title',
-        'collection.description',
-        'collection.createdAt',
-        'collection_keyword',
-      ])
-      .leftJoin('collection.user', 'user')
-      .leftJoin('collection.photospots', 'photospot')
-      .leftJoin('collection.collection_keywords', 'collection_keyword')
-      .orderBy('collection.id', 'DESC')
+    let whereQuery = this.isThereSearchUserid(search, userId)
+    return await this.createQueryBuilder('c')
+      .where( whereQuery.q1, whereQuery.q2)
+      .select(['c.id', 'c.userId', 'c.title', 'c.description', 'c.createdAt', 'ck'])
+      .leftJoin('c.user', 'cu')
+      .leftJoin('c.photospots', 'cp')
+      .leftJoin('c.collection_keywords', 'ck')
+      .orderBy('c.id', 'DESC')
       .take(take)
       .skip((p - 1) * take)
       .getMany()
   }
 
-  async getCollection(collectionId: number): Promise<Collection | null> {
-    return await this.createQueryBuilder('collection')
-      .select([
-        'collection.id',
-        'collection.userId',
-        'collection.title',
-        'collection.description',
-        'collection.createdAt',
-        'collection_keyword',
-      ])
-      .leftJoin('collection.user', 'user')
-      .leftJoin('collection.photospots', 'photospot')
-      .leftJoin('collection.collection_keywords', 'collection_keyword')
-      .where('collection.id = :id', { id: collectionId })
-      .getOne();
+  isThereSearchUserid(search?: string, userId?: number) {
+    let myCollectionQuery = 'c.userId = :userId';
+    let searchCollectionQuery = 'c.title LIKE :search OR c.description LIKE :search';
+    let query = { q1: '', q2: {} };
+
+    if (search && !userId) {
+      query.q1 = searchCollectionQuery
+      query.q2 = { search: `%${search}%` }
+    } else if (!search && userId) {
+      query.q1 = myCollectionQuery
+      query.q2 = { userId }
+    } else if (search && userId) {
+      query.q1 = `${myCollectionQuery} AND (${searchCollectionQuery})`
+      query.q2 = { userId, search: `%${search}%` }
+    } return query
   }
-}
+
+  async getCollection(collectionId: number): Promise<Collection | null> {
+    return await this.createQueryBuilder('c')
+      .where('c.id = :id', { id: collectionId })
+      .select(['c.id', 'c.userId', 'c.title', 'c.description', 'c.createdAt', 'ck'])
+      .leftJoin('c.user', 'cu')
+      .leftJoin('c.photospots', 'cp')
+      .leftJoin('c.collection_keywords', 'ck')
+      .getOne();
+    }
+  }
+
