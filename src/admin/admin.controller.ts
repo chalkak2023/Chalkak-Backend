bbcc204f93c27417372a3582da069f90f276b766
boleesystem <boleesystem@gmail.com>
2023-03-14 18:00:50 +0900

    fix: 전 메소드에 대해 UseGuards(jwt-admin) 적용(로그인, 토큰 관련 제외)

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -21,11 +21,13 @@ export class AdminController {
 
   // 관리자 관리
   @Get('auth')
+  @UseGuards(AuthGuard('jwt-admin'))
   async getAdminsList(@Query('search') search: string, @Query('p') p: number = 1): Promise<Admin[]> {
     return await this.adminService.getAdminsList(search, p);
   }
 
   @Post('auth/signup')
+  @UseGuards(AuthGuard('jwt-admin'))
   async signupAdmin(@Body() data: SignupAdminReqDto) {
     return await this.adminService.signupAdmin(data);
   }
@@ -55,22 +57,26 @@ export class AdminController {
   }
 
   @Delete('auth/:id')
+  @UseGuards(AuthGuard('jwt-admin'))
   async deleteAdmin(@Param('id') id: number) {
     return this.adminService.deleteAdmin(id);
   }
 
   @Post('auth/signout')
+  @UseGuards(AuthGuard('jwt-admin'))
   signoutAdmin(@Req() req: Request, @Res() res: Response): any {
     return res.send({ message: '정상적으로 로그아웃하였습니다.' });
   }
 
   // 유저 관리
   @Get('users')
+  @UseGuards(AuthGuard('jwt-admin'))
   async getAdminUsersList(@Query('search') search: string, @Query('p') p: number = 1): Promise<User[]> {
     return await this.adminService.getAdminUsersList(search, p);
   }
 
   @Put('users/:id')
+  @UseGuards(AuthGuard('jwt-admin'))
   async blockAdminUser(@Param('id') id: string, @Body() blockUser: BlockAdminUserDto) {
     const { isBlock } = blockUser;
     return await this.adminService.blockAdminUser(id, { isBlock: !isBlock });
@@ -78,64 +84,76 @@ export class AdminController {
 
   // 콜렉션 관리
   @Get('collections')
+  @UseGuards(AuthGuard('jwt-admin'))
   async getAdminCollectionsList(@Query('search') search: string, @Query('p') p: number = 1): Promise<Collection[]> {
     return await this.adminService.getAdminCollectionsList(search, p);
   }
 
   @Delete('collections/:id')
+  @UseGuards(AuthGuard('jwt-admin'))
   async deleteAdminCollection(@Param('id') id: number) {
     return await this.adminService.deleteAdminCollection(id);
   }
 
   // 포토스팟 관리
   @Get(':collectionId/photospots')
+  @UseGuards(AuthGuard('jwt-admin'))
   async getAdminPhotospotList(@Param('collectionId') collectionId: number): Promise<Photospot[]> {
     return this.adminService.getAdminPhotospotList(collectionId);
   }
 
   @Get(':collectionId/photospots/:photospotId')
+  @UseGuards(AuthGuard('jwt-admin'))
   async getAdminPhotospot(@Param('photospotId') photospotId: number): Promise<Photospot> {
     return this.adminService.getAdminPhotospot(photospotId);
   }
 
   @Delete(':collectionId/photospots/:photospotId')
+  @UseGuards(AuthGuard('jwt-admin'))
   async deleteAdminPhotospot(@Param('photospotId') photospotId: number) {
     await this.adminService.deleteAdminPhotospot(photospotId);
   }
 
   // 모임 관리
   @Get('meetups')
+  @UseGuards(AuthGuard('jwt-admin'))
   async getAdminMeetupsList(@Query('search') search: string, @Query('p') p: number = 1): Promise<Meetup[]> {
     return await this.adminService.getAdminMeetupsList(search, p);
   }
 
   @Delete('meetups/:id')
+  @UseGuards(AuthGuard('jwt-admin'))
   async deleteAdminMeetup(@Param('id') id: number) {
     return await this.adminService.deleteAdminMeetup(id);
   }
 
   // 자주찾는질문 관리
   @Get('faq')
+  @UseGuards(AuthGuard('jwt-admin'))
   async getAdminFaqList(@Query('search') search: string, @Query('p') p: number = 1): Promise<Faq[]> {
     return await this.adminService.getAdminFaqList(search, p);
   }
 
   @Get('faq/:id')
+  @UseGuards(AuthGuard('jwt-admin'))
   async getAdminFaq(@Param('id') id: number): Promise<Faq> {
     return this.adminService.getAdminFaq(id);
   }
 
   @Post('faq')
+  @UseGuards(AuthGuard('jwt-admin'))
   async createAdminFaq(@Body() data: CreateAdminFaqDto) {
     return await this.adminService.createAdminFaq(data);
   }
 
   @Put('faq/:id')
+  @UseGuards(AuthGuard('jwt-admin'))
   async updateAdminFaq(@Param('id') id: number, @Body() updateAdminFaqtDto: UpdateAdminFaqDto) {
     await this.adminService.updateAdminFaq(updateAdminFaqtDto, id);
   }
 
   @Delete('faq/:id')
+  @UseGuards(AuthGuard('jwt-admin'))
   async deleteAdminFaq(@Param('id') id: number) {
     return await this.adminService.deleteAdminFaq(id);
   }
