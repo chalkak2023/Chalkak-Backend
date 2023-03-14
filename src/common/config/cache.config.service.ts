58d8ae63cbb009bbc33e5fd8ee587620648d2d47
Park Jin <tstunas@gmail.com>
2023-03-14 22:27:37 +0900

    feat: 레디스를 캐시 스토어로 사용할 때 username, password로 쓸 수 있는 환경변수 추가

--- src/common/config/cache.config.service.ts
+++ src/common/config/cache.config.service.ts
@@ -10,6 +10,20 @@ export class CacheConfigService implements CacheOptionsFactory {
   createCacheOptions(): CacheModuleOptions<StoreConfig> {
     const store = this.configService.get('CACHE_STORE_NAME') || 'memory'
     if (store === 'redis') {
+      const username = this.configService.get('CACHE_REDIS_USERNAME');
+      const password = this.configService.get('CACHE_REDIS_PASSWORD');
+      const config: CacheModuleOptions<StoreConfig> = {
+        store: redisStore,
+        host: this.configService.get('CACHE_HOST') || 'localhost',
+        port: this.configService.get('CACHE_PORT') ? Number(this.configService.get('CACHE_PORT')) : 6379,
+        ttl: this.configService.get('CACHE_TTL') ? Number(this.configService.get('CACHE_REDIS_TTL')) : 5,
+      }
+      if (username) {
+        config.username = username
+      }
+      if (password) {
+        config.passowd = password
+      }
       return {
         store: redisStore,
         host: this.configService.get('CACHE_HOST') || 'localhost',
