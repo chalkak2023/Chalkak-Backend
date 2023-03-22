8c06f367478b39020f8b03702fa4516c6b6f95ce
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-03-22 15:39:21 +0900

    feat: chat - 채팅방 기능 추가

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,6 +1,8 @@
 import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Namespace, Socket } from 'socket.io';
-import { ChatDTO } from './dto/chat.dto';
+import { ChatDTO, JoinLeaveChatDTO } from './dto/chat.dto';
+
+let createdRooms: string[] = [];
 
 @WebSocketGateway({
   namespace: 'chat',
@@ -14,51 +16,81 @@ export class ChatGateway
 
   // 초기화 이후에 실행
   afterInit() {
-    this.nsp.adapter.on('create-room', (room) => {
-      console.log(`"Room:${room}"이 생성되었습니다.`);
+    this.nsp.adapter.on('delete-room', (roomId) => {
+
+      const deletedRoom = createdRooms.find(
+        (createdRoom) => createdRoom === roomId
+      );
+      if (!deletedRoom) return;
+
+      this.nsp.emit('delete-room', deletedRoom);
+      createdRooms = createdRooms.filter(
+        (createdRoom) => createdRoom !== deletedRoom,
+      );
+
+      // console.log(`"Room:${roomId}"이 삭제되었습니다.`);
     });
-
-
-    this.nsp.adapter.on('join-room', (room, id) => {
-      console.log(`"Socket:${id}"이 "Room:${room}"에 참여하였습니다.`);
-    });
-
-
-    this.nsp.adapter.on('leave-room', (room, id) => {
-      console.log(`"Socket:${id}"이 "Room:${room}"에서 나갔습니다.`);
-    });
-
-
-    this.nsp.adapter.on('delete-room', (roomName) => {
-      console.log(`"Room:${roomName}"이 삭제되었습니다.`);
-    });
-
 
     console.log('웹소켓 서버 초기화 ✅');
   }
 
   // 소켓이 연결되면 실행
   handleConnection(@ConnectedSocket() socket: Socket) {
-    console.log(`${socket.id} 소켓 연결`);
+    // console.log(`${socket.id} 소켓 연결`);
 
-    const alert = { message: `${socket.id}가 들어왔습니다.` };
-    socket.broadcast.emit('alert', { alert });
+    // const alert = { message: `${socket.id}가 들어왔습니다.` };
+    // socket.broadcast.emit('alert', { alert });
   }
 
   // 소켓 연결이 끊기면 실행
   handleDisconnect(@ConnectedSocket() socket: Socket) {
-    console.log(`${socket.id} 소켓 연결 해제 ❌`);
+    // console.log(`${socket.id} 소켓 연결 해제 ❌`);
 
-    const alert = { message: `${socket.id}가 퇴장하셨습니다.` };
-    socket.broadcast.emit('alert', { alert });
+    // const alert = { message: `${socket.id}가 나갔습니다.` };
+    // socket.broadcast.emit('alert', { alert });
   }
 
   @SubscribeMessage('message')
   handleMessage(
     @ConnectedSocket() socket: Socket,
-    @MessageBody() msgObj: ChatDTO,
+    @MessageBody() chatObj: ChatDTO,
   ) {
-    socket.broadcast.emit('message', { msgObj });
-    return { msgObj };
+    socket.broadcast
+      .to(chatObj.roomId)
+      .emit('message', { chatObj: chatObj });
+    return { chatObj };
+  }
+
+  @SubscribeMessage('join-room')
+  handleJoinRoom(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() chatObj: JoinLeaveChatDTO,
+  ) {
+    const exists = createdRooms.find((createdRoom) => createdRoom === chatObj.roomId);
+    if (exists) {
+      // 이미 방이 존재할 경우 입장
+      socket.join(chatObj.roomId); 
+      const alert = { message: `${chatObj.username}님이 들어왔습니다.` };
+      socket.broadcast
+        .to(chatObj.roomId)
+        .emit('alert', { alert });
+      return { success: true, payload: chatObj.roomId };
+    }
+    socket.join(chatObj.roomId);
+    createdRooms.push(chatObj.roomId);
+    return { success: true, payload: chatObj.roomId };
+  }
+
+  @SubscribeMessage('leave-room')
+  handleLeaveRoom(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() chatObj: JoinLeaveChatDTO,
+    ) {
+    socket.leave(chatObj.roomId);
+    const alert = { message: `${chatObj.username}님이 나갔습니다.` };
+    socket.broadcast
+      .to(chatObj.roomId)
+      .emit('alert', { alert });
+    return { success: true };
   }
 }
--- src/chat/dto/chat.dto.ts
+++ src/chat/dto/chat.dto.ts
@@ -1,9 +1,16 @@
+import { PickType } from "@nestjs/mapped-types";
 import { Type } from "class-transformer";
 import { IsDate, IsNumber, IsString } from "class-validator";
 
 export class ChatDTO {
+  // @IsNumber()  
+  // readonly roomId: number;
+  // socket.join은 string만 받는데 meetupId (number) 로 전달해야 하므로 일단 stirng으로 변환되게 선언
+  @IsString()  
+  readonly roomId: string;
+
   @IsNumber()
-  userId: number;
+  readonly userId: number;
 
   @IsString()
   readonly username: string;
@@ -15,3 +22,5 @@ export class ChatDTO {
   @IsDate()
   readonly createdAt: Date;
 }
+
+export class JoinLeaveChatDTO extends PickType(ChatDTO, ['roomId', 'userId', 'username'] as const) {}
